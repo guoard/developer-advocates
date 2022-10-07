80bcc49df13bd2a9f1b979eef1763e7859e2fafe
Ali Afsharzadeh <afsharzadeh1998@gmail.com>
2022-10-07 20:15:51 +0330

    Use row id instead of uuid

--- utils/models.py
+++ utils/models.py
@@ -1,8 +1,6 @@
-import uuid
 from django.db import models
 
 class BaseModel(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     updated_at = models.DateTimeField(auto_now=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
