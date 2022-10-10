ced8d556028f504c8af5c86e4ee88c1d93820120
Ali Afsharzadeh <afsharzadeh1998@gmail.com>
2022-10-10 23:28:07 +0330

    Use view set instead of cbv

--- advocates/serializers.py
+++ advocates/serializers.py
@@ -1,4 +1,5 @@
 from django.urls import reverse
+from django.db import transaction
 from rest_framework import serializers
 
 from companies.models import Company
@@ -31,25 +32,59 @@ class AdvocateSerializer(serializers.ModelSerializer):
         fields = ['id', 'name', 'short_bio', 'long_bio',
                   'profile_pic', 'advocate_years_exp', 'links', 'company']
 
-class AddAdvocateSerializer(serializers.ModelSerializer):
+
+class CreateAdvocateSerializer(serializers.ModelSerializer):
     links = SocialMediaSerializer()
-    company_id = serializers.IntegerField(initial=None, min_value=1, allow_null=True)
+    company_id = serializers.IntegerField(
+        initial=None, min_value=1, allow_null=True)
 
     def validate_company_id(self, value):
         if value and not Company.objects.filter(pk=value).exists():
-            raise serializers.ValidationError('No company with given ID was found.')
+            raise serializers.ValidationError(
+                'No company with given ID was found.')
         return value
 
     def create(self, validated_data):
-        company = Company.objects.get(id=validated_data.pop('company_id'))
-        links = validated_data.pop('links')
+        with transaction.atomic():
+            company_id = self.validated_data.pop('company_id')
+            try:
+                company = Company.objects.get(id=company_id)
+            except Company.DoesNotExist:
+                company = None
 
-        advocate = Advocate.objects.create(company=company, **validated_data)
-        SocialMedia.objects.create(advocate=advocate, **links)
-        return advocate
+            links = validated_data.pop('links')
 
+            advocate = Advocate.objects.create(
+                company=company, **validated_data)
+            SocialMedia.objects.create(advocate=advocate, **links)
+            return advocate
+
+    def update(self, instance: Advocate, validated_data):
+        with transaction.atomic():
+            company_id = self.validated_data.pop('company_id')
+            try:
+                company = Company.objects.get(id=company_id)
+            except Company.DoesNotExist:
+                company = None
+
+            SocialMedia.objects.update_or_create(
+                advocate=instance, defaults=validated_data.pop('links'))
+
+            instance.name = validated_data.get('name', instance.name)
+            instance.short_bio = validated_data.get(
+                'short_bio', instance.short_bio)
+            instance.long_bio = validated_data.get(
+                'long_bio', instance.long_bio)
+            instance.profile_pic = validated_data.get(
+                'profile_pic', instance.profile_pic)
+            instance.advocate_years_exp = validated_data.get(
+                'advocate_years_exp', instance.advocate_years_exp)
+            instance.company = company
+            instance.save()
+
+            return instance
 
     class Meta:
         model = Advocate
-        fields = ['name', 'short_bio', 'long_bio',
+        fields = ['id', 'name', 'short_bio', 'long_bio',
                   'profile_pic', 'advocate_years_exp', 'links', 'company_id']
--- advocates/views.py
+++ advocates/views.py
@@ -1,14 +1,24 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
 
 from .models import Advocate
-from .serializers import AdvocateSerializer, AddAdvocateSerializer
+from .serializers import AdvocateSerializer, CreateAdvocateSerializer
 
 
 class AdvocateViewSet(ModelViewSet):
+    allow_http_methods = ['get', 'post', 'put', 'delete']
+
+    def update(self, request, *args, **kwargs):
+        serializer = CreateAdvocateSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        advocate = serializer.save()
+        serializer = AdvocateSerializer(advocate)
+        return Response(serializer.data)
+
     queryset = Advocate.objects.select_related(
         'company').prefetch_related('links').all()
 
     def get_serializer_class(self):
-        if self.request.method == 'POST':
-            return AddAdvocateSerializer
+        if self.request.method == 'POST' or self.request.method == 'PUT':
+            return CreateAdvocateSerializer
         return AdvocateSerializer
